48614e68755784abf39bfbba680bd4d0deb09e41
Арсен Гиоев <gioev.arsen15@list.ru>
2023-05-12 01:21:49 +0300

    Кнопки на форме этапа3 теперь работают. Исправлено примечание. Форма этапа3 пока не закрывается автоматически. Требуются некоторые исправления в коде.

--- Lüscher Color Test/FormStage1.Designer.cs
+++ Lüscher Color Test/FormStage1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormStage1));
             this.labelStage1 = new System.Windows.Forms.Label();
             this.buttonCard1 = new System.Windows.Forms.Button();
             this.buttonCard2 = new System.Windows.Forms.Button();
@@ -70,6 +71,7 @@
             this.Controls.Add(this.buttonCard2);
             this.Controls.Add(this.buttonCard1);
             this.Controls.Add(this.labelStage1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FormStage1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- Lüscher Color Test/FormStage2.Designer.cs
+++ Lüscher Color Test/FormStage2.Designer.cs
@@ -30,14 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormStage2));
             this.labelStage2 = new System.Windows.Forms.Label();
-            this.buttonCard2_1 = new System.Windows.Forms.Button();
-            this.buttonCard2_2 = new System.Windows.Forms.Button();
-            this.buttonCard2_3 = new System.Windows.Forms.Button();
-            this.buttonCard2_4 = new System.Windows.Forms.Button();
-            this.buttonCard2_5 = new System.Windows.Forms.Button();
-            this.buttonCard2_6 = new System.Windows.Forms.Button();
-            this.buttonCard2_7 = new System.Windows.Forms.Button();
-            this.buttonCard2_8 = new System.Windows.Forms.Button();
+            this.buttonCard1 = new System.Windows.Forms.Button();
+            this.buttonCard2 = new System.Windows.Forms.Button();
+            this.buttonCard3 = new System.Windows.Forms.Button();
+            this.buttonCard4 = new System.Windows.Forms.Button();
+            this.buttonCard5 = new System.Windows.Forms.Button();
+            this.buttonCard6 = new System.Windows.Forms.Button();
+            this.buttonCard7 = new System.Windows.Forms.Button();
+            this.buttonCard8 = new System.Windows.Forms.Button();
             this.labelNote = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -51,69 +51,85 @@
             this.labelStage2.Text = "Этап 2.  По очереди выбирайте наиболее привлекательный Вам цвет.";
             this.labelStage2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // buttonCard2_1
+            // buttonCard1
             // 
-            this.buttonCard2_1.Location = new System.Drawing.Point(60, 175);
-            this.buttonCard2_1.Name = "buttonCard2_1";
-            this.buttonCard2_1.Size = new System.Drawing.Size(100, 150);
-            this.buttonCard2_1.TabIndex = 2;
-            this.buttonCard2_1.UseVisualStyleBackColor = true;
+            this.buttonCard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCard1.Location = new System.Drawing.Point(60, 175);
+            this.buttonCard1.Name = "buttonCard1";
+            this.buttonCard1.Size = new System.Drawing.Size(100, 150);
+            this.buttonCard1.TabIndex = 2;
+            this.buttonCard1.UseVisualStyleBackColor = true;
+            this.buttonCard1.Click += new System.EventHandler(this.AnyButton_Click);
             // 
-            // buttonCard2_2
+            // buttonCard2
             // 
-            this.buttonCard2_2.Location = new System.Drawing.Point(197, 175);
-            this.buttonCard2_2.Name = "buttonCard2_2";
-            this.buttonCard2_2.Size = new System.Drawing.Size(100, 150);
-            this.buttonCard2_2.TabIndex = 3;
-            this.buttonCard2_2.UseVisualStyleBackColor = true;
+            this.buttonCard2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCard2.Location = new System.Drawing.Point(197, 175);
+            this.buttonCard2.Name = "buttonCard2";
+            this.buttonCard2.Size = new System.Drawing.Size(100, 150);
+            this.buttonCard2.TabIndex = 3;
+            this.buttonCard2.UseVisualStyleBackColor = true;
+            this.buttonCard2.Click += new System.EventHandler(this.AnyButton_Click);
             // 
-            // buttonCard2_3
+            // buttonCard3
             // 
-            this.buttonCard2_3.Location = new System.Drawing.Point(334, 175);
-            this.buttonCard2_3.Name = "buttonCard2_3";
-            this.buttonCard2_3.Size = new System.Drawing.Size(100, 150);
-            this.buttonCard2_3.TabIndex = 4;
-            this.buttonCard2_3.UseVisualStyleBackColor = true;
+            this.buttonCard3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCard3.Location = new System.Drawing.Point(334, 175);
+            this.buttonCard3.Name = "buttonCard3";
+            this.buttonCard3.Size = new System.Drawing.Size(100, 150);
+            this.buttonCard3.TabIndex = 4;
+            this.buttonCard3.UseVisualStyleBackColor = true;
+            this.buttonCard3.Click += new System.EventHandler(this.AnyButton_Click);
             // 
-            // buttonCard2_4
+            // buttonCard4
             // 
-            this.buttonCard2_4.Location = new System.Drawing.Point(471, 175);
-            this.buttonCard2_4.Name = "buttonCard2_4";
-            this.buttonCard2_4.Size = new System.Drawing.Size(100, 150);
-            this.buttonCard2_4.TabIndex = 5;
-            this.buttonCard2_4.UseVisualStyleBackColor = true;
+            this.buttonCard4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCard4.Location = new System.Drawing.Point(471, 175);
+            this.buttonCard4.Name = "buttonCard4";
+            this.buttonCard4.Size = new System.Drawing.Size(100, 150);
+            this.buttonCard4.TabIndex = 5;
+            this.buttonCard4.UseVisualStyleBackColor = true;
+            this.buttonCard4.Click += new System.EventHandler(this.AnyButton_Click);
             // 
-            // buttonCard2_5
+            // buttonCard5
             // 
-            this.buttonCard2_5.Location = new System.Drawing.Point(608, 175);
-            this.buttonCard2_5.Name = "buttonCard2_5";
-            this.buttonCard2_5.Size = new System.Drawing.Size(100, 150);
-            this.buttonCard2_5.TabIndex = 6;
-            this.buttonCard2_5.UseVisualStyleBackColor = true;
+            this.buttonCard5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCard5.Location = new System.Drawing.Point(608, 175);
+            this.buttonCard5.Name = "buttonCard5";
+            this.buttonCard5.Size = new System.Drawing.Size(100, 150);
+            this.buttonCard5.TabIndex = 6;
+            this.buttonCard5.UseVisualStyleBackColor = true;
+            this.buttonCard5.Click += new System.EventHandler(this.AnyButton_Click);
             // 
-            // buttonCard2_6
+            // buttonCard6
             // 
-            this.buttonCard2_6.Location = new System.Drawing.Point(745, 175);
-            this.buttonCard2_6.Name = "buttonCard2_6";
-            this.buttonCard2_6.Size = new System.Drawing.Size(100, 150);
-            this.buttonCard2_6.TabIndex = 7;
-            this.buttonCard2_6.UseVisualStyleBackColor = true;
+            this.buttonCard6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCard6.Location = new System.Drawing.Point(745, 175);
+            this.buttonCard6.Name = "buttonCard6";
+            this.buttonCard6.Size = new System.Drawing.Size(100, 150);
+            this.buttonCard6.TabIndex = 7;
+            this.buttonCard6.UseVisualStyleBackColor = true;
+            this.buttonCard6.Click += new System.EventHandler(this.AnyButton_Click);
             // 
-            // buttonCard2_7
+            // buttonCard7
             // 
-            this.buttonCard2_7.Location = new System.Drawing.Point(882, 175);
-            this.buttonCard2_7.Name = "buttonCard2_7";
-            this.buttonCard2_7.Size = new System.Drawing.Size(100, 150);
-            this.buttonCard2_7.TabIndex = 8;
-            this.buttonCard2_7.UseVisualStyleBackColor = true;
+            this.buttonCard7.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCard7.Location = new System.Drawing.Point(882, 175);
+            this.buttonCard7.Name = "buttonCard7";
+            this.buttonCard7.Size = new System.Drawing.Size(100, 150);
+            this.buttonCard7.TabIndex = 8;
+            this.buttonCard7.UseVisualStyleBackColor = true;
+            this.buttonCard7.Click += new System.EventHandler(this.AnyButton_Click);
             // 
-            // buttonCard2_8
+            // buttonCard8
             // 
-            this.buttonCard2_8.Location = new System.Drawing.Point(1015, 175);
-            this.buttonCard2_8.Name = "buttonCard2_8";
-            this.buttonCard2_8.Size = new System.Drawing.Size(100, 150);
-            this.buttonCard2_8.TabIndex = 9;
-            this.buttonCard2_8.UseVisualStyleBackColor = true;
+            this.buttonCard8.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCard8.Location = new System.Drawing.Point(1015, 175);
+            this.buttonCard8.Name = "buttonCard8";
+            this.buttonCard8.Size = new System.Drawing.Size(100, 150);
+            this.buttonCard8.TabIndex = 9;
+            this.buttonCard8.UseVisualStyleBackColor = true;
+            this.buttonCard8.Click += new System.EventHandler(this.AnyButton_Click);
             // 
             // labelNote
             // 
@@ -122,26 +138,25 @@
             this.labelNote.ForeColor = System.Drawing.Color.Red;
             this.labelNote.Location = new System.Drawing.Point(56, 512);
             this.labelNote.Name = "labelNote";
-            this.labelNote.Size = new System.Drawing.Size(983, 21);
+            this.labelNote.Size = new System.Drawing.Size(823, 21);
             this.labelNote.TabIndex = 10;
-            this.labelNote.Text = "Примечание: уже выбранная карточка будет помечена крестиком и станет некликабельн" +
-    "а до следующей комбинации.";
+            this.labelNote.Text = "Примечание: уже выбранная карточка пропадет и станет недоступна до следующей комб" +
+    "инации.";
             // 
             // FormStage2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 561);
-            this.ControlBox = false;
             this.Controls.Add(this.labelNote);
-            this.Controls.Add(this.buttonCard2_8);
-            this.Controls.Add(this.buttonCard2_7);
-            this.Controls.Add(this.buttonCard2_6);
-            this.Controls.Add(this.buttonCard2_5);
-            this.Controls.Add(this.buttonCard2_4);
-            this.Controls.Add(this.buttonCard2_3);
-            this.Controls.Add(this.buttonCard2_2);
-            this.Controls.Add(this.buttonCard2_1);
+            this.Controls.Add(this.buttonCard8);
+            this.Controls.Add(this.buttonCard7);
+            this.Controls.Add(this.buttonCard6);
+            this.Controls.Add(this.buttonCard5);
+            this.Controls.Add(this.buttonCard4);
+            this.Controls.Add(this.buttonCard3);
+            this.Controls.Add(this.buttonCard2);
+            this.Controls.Add(this.buttonCard1);
             this.Controls.Add(this.labelStage2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -156,14 +171,14 @@
         #endregion
 
         private System.Windows.Forms.Label labelStage2;
-        private System.Windows.Forms.Button buttonCard2_1;
-        private System.Windows.Forms.Button buttonCard2_2;
-        private System.Windows.Forms.Button buttonCard2_3;
-        private System.Windows.Forms.Button buttonCard2_4;
-        private System.Windows.Forms.Button buttonCard2_5;
-        private System.Windows.Forms.Button buttonCard2_6;
-        private System.Windows.Forms.Button buttonCard2_7;
-        private System.Windows.Forms.Button buttonCard2_8;
+        private System.Windows.Forms.Button buttonCard1;
+        private System.Windows.Forms.Button buttonCard2;
+        private System.Windows.Forms.Button buttonCard3;
+        private System.Windows.Forms.Button buttonCard4;
+        private System.Windows.Forms.Button buttonCard5;
+        private System.Windows.Forms.Button buttonCard6;
+        private System.Windows.Forms.Button buttonCard7;
+        private System.Windows.Forms.Button buttonCard8;
         private System.Windows.Forms.Label labelNote;
     }
 }
